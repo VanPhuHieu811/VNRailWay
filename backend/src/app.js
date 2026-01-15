103559bc27bdc3c050517319c2e2835202fc6ee8
duyhao7777 <daoduyhao7777@gmail.com>
2026-01-15 19:09:06 +0700

    edit app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-// Import Route vừa tạo
-import bookingRoutes from './routes/booking.route.js'; 
-import ticketRoutes from './routes/ticket.route.js';
-import adminRoutes from './routes/admin.route.js';
 
 
 const app = express();
@@ -15,10 +11,5 @@ app.use(express.urlencoded({ extended: true }));
 
 const apiV1Router = express.Router();
 
-apiV1Router.use('/booking', bookingRoutes); // -> /api/v1/booking/dat-ve-demo
-apiV1Router.use('/tickets', ticketRoutes);  // -> /api/v1/tickets/{code}
-apiV1Router.use('/admin', adminRoutes);      // -> /api/v1/admin/schedules
-
-app.use('/api/v1', apiV1Router);
 
 export default app;
